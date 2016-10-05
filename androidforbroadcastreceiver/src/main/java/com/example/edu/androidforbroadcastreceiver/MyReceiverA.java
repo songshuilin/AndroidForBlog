2fe90cbfa9adefaa3dc00262e825940d85d19f5e
songshuilin <631659521@qq.com>
2016-10-05 23:10:00 +0800

    android  普通广播和有序广播

---
+++ androidforbroadcastreceiver/src/main/java/com/example/edu/androidforbroadcastreceiver/MyReceiverA.java
@@ -0,0 +1,15 @@
+package com.example.edu.androidforbroadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyReceiverA extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i("TAG", " MyReceiverA 广播 ");
+        abortBroadcast();
+    }
+}
